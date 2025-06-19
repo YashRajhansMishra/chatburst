1a20096fafd1f24aa4d6426a92286cd9c431de80
Yash Mishra <mishrayash060@gmail.com>
2025-06-19 19:01:41 +0530

    Add files via upload

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '960ce6dd356840039cdc6cbacd23af46';
+  static String appcertificate = '2d5da8c456ec4b66b5100ca72cf4054e';
+}
